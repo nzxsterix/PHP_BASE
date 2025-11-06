a806901e19f5810da8ad70592bd1d0adea15f39c
Stephan Geraci <nzxsterix@gmail.com>
2025-11-06 16:42:56 +0100

    Aggiunto Es sommaNumeri

---
+++ sommaNumeri/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Somma Numeri</title>
+</head>
+<body>
+
+    <?php
+
+        // Inizializzo la somma a zero
+        $somma = 0;
+        $numeri = [1,2,3,4,5,6,7,8,9,10];
+    
+        // Ciclo attraverso i numeri da 1 a 10 e li sommo
+        for ($i = 1; $i <= 10; $i++) {
+            $somma += $i;
+        }
+
+        // Stampo il risultato
+        echo "<p>La somma dei numeri da 1 a 10 è: $somma</p>";
+    ?>
+    
+</body>
+</html>
